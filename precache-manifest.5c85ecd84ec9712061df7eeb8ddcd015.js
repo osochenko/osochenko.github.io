0925e24be8e472edb17a5a62d04c3d77720c70c6
Vadim Osochenko <vadim@magpaylabs.com>
2018-09-03 10:47:59 +0300

    deploy

---
+++ precache-manifest.5c85ecd84ec9712061df7eeb8ddcd015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7f582241cb6767f4bf81",
+    "url": "/js/chunk-vendors.f1656cfe.js"
+  },
+  {
+    "revision": "f1face7fb3a49c27daf5",
+    "url": "/js/app.7af7ecae.js"
+  },
+  {
+    "revision": "941c651b2da1afecb938c8c0ef822b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f1b50d9f9cb28f08a3122a31c54066",
+    "url": "/img/login-bg.d7f1b50d.jpg"
+  },
+  {
+    "revision": "7f582241cb6767f4bf81",
+    "url": "/css/chunk-vendors.c57a9491.css"
+  },
+  {
+    "revision": "f1face7fb3a49c27daf5",
+    "url": "/css/app.800e4ca4.css"
+  }
+];
